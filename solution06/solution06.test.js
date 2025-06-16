a6ab7aad063483eadcdd127ba70bae44a3807417
jashka34 <a_cherniadiev@mail.ru>
2025-06-16 06:26:24 +0300

    06/50

---
+++ solution06/solution06.test.js
@@ -0,0 +1,10 @@
+import { expect, test, vi } from "vitest";
+
+test("hello world", async () => {
+  const consoleLogSpy = vi.spyOn(console, "log").mockImplementation(() => {});
+  await import("./solution06.js");
+
+  const firstArg = consoleLogSpy.mock.calls.join("\n");
+
+  expect(firstArg).toBe("9");
+});
