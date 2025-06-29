4a2c42e3120e805f7f15099dc216aad787e1708a
jashka34 <a_cherniadiev@mail.ru>
2025-06-29 12:19:52 +0300

    19/50

---
+++ solution19/solution19.test.js
@@ -0,0 +1,10 @@
+import { expect, test, vi } from "vitest";
+
+test("hello world", async () => {
+  const consoleLogSpy = vi.spyOn(console, "log").mockImplementation(() => {});
+  await import("./solution19.js");
+
+  const firstArg = consoleLogSpy.mock.calls.join("\n");
+
+  expect(firstArg).toBe("What Is Dead May Never Die!");
+});
