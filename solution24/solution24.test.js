ea02c738a79db40eac881f82fad2439ebcc38f3e
jashka34 <a_cherniadiev@mail.ru>
2025-07-09 21:55:46 +0300

    24/50 start

---
+++ solution24/solution24.test.js
@@ -0,0 +1,12 @@
+import { expect, test, vi } from "vitest";
+
+test("hello world", async () => {
+  const consoleLogSpy = vi.spyOn(console, "log").mockImplementation(() => {});
+  await import("./index.js");
+
+  const firstArg = consoleLogSpy.mock.calls.join("\n");
+
+  expect(firstArg).toBe(
+    "Hello, Joffrey!\nHere is important information about your account security.\nWe couldn't verify your mother's maiden name.",
+  );
+});
