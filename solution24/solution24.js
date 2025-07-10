2c7ac3fa0956edc219c00563f8b68c61b7990051
jashka34 <a_cherniadiev@mail.ru>
2025-07-10 19:34:34 +0300

    24/50

--- solution24/solution24.js
+++ solution24/solution24.js
@@ -1,1 +1,10 @@
-console.log("1");
+let info = "We couldn't verify your mother's maiden name.";
+let intro = "Here is important information about your account security.";
+
+let firstName = "Joffrey";
+let greeting = "Hello";
+
+// BEGIN (write your solution here)
+
+console.log(greeting + ", " + firstName + "!\n" + intro + "\n" + info);
+// END
