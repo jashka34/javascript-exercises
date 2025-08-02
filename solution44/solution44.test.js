ac95ec16f5f5a156209334754f4b98faec517d3c
jashka34 <a_cherniadiev@mail.ru>
2025-08-02 10:17:02 +0300

    44/50

---
+++ solution44/solution44.test.js
@@ -0,0 +1,12 @@
+import { expect, test, vi } from "vitest";
+
+test("hello world", async () => {
+  const consoleLogSpy = vi.spyOn(console, "log").mockImplementation(() => {});
+  await import("./solution44.js");
+
+  const firstArg = consoleLogSpy.mock.calls.join("\n");
+
+  expect(firstArg).toBe(
+    "a mind needs books as a sword needs a whetstone, if it is to keep its edge.",
+  );
+});
