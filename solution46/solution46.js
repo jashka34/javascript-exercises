a28ea57bdbf909f2639e4da66ae7525a96082468
jashka34 <a_cherniadiev@mail.ru>
2025-08-09 09:05:24 +0300

    47/50

--- solution46/solution46.js
+++ solution46/solution46.js
@@ -1,5 +1,5 @@
 const text = "Never forget what you are, for surely the world will not";
 // BEGIN (write your solution here)
-firstName = firstName.trim();
-console.log(firstName);
+console.log(`First: ${text[0]}`);
+console.log(`Last: ${text[text.length - 1]}`);
 // END
