f0fff625f396863b1284ff2babee27488732fd59
jashka34 <a_cherniadiev@mail.ru>
2025-08-13 06:25:22 +0300

    48/72

---
+++ solution48/solution48.test.js
@@ -0,0 +1,11 @@
+import { expect, test, vi } from "vitest";
+import f from "./solution48.js";
+
+test("hello world", async () => {
+  const consoleLogSpy = vi.spyOn(console, "log").mockImplementation(() => {});
+  f();
+
+  const firstArg = consoleLogSpy.mock.calls.join("\n");
+
+  expect(firstArg).toBe("Winter is coming");
+});
