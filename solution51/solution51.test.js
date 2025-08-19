3128f483488176d4b5fa7e7a3b50f19c9eb9db32
jashka34 <a_cherniadiev@mail.ru>
2025-08-19 21:02:15 +0300

    51/72 begin

---
+++ solution51/solution51.test.js
@@ -0,0 +1,9 @@
+import { expect, test } from "vitest";
+import f from "./solution51.js";
+
+test("test", () => {
+  expect(f("1234123412341234")).toEqual("****1234");
+  expect(f("1234123412344321")).toEqual("****4321");
+  expect(f("1234123412344321", 2)).toEqual("**4321");
+  expect(f("1234123412341234", 12)).toEqual("************1234");
+});
