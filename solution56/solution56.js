1b1f8866f889706389155f03ee69ae17ad1dada3
jashka34 <a_cherniadiev@mail.ru>
2025-08-29 05:42:49 +0300

    56/72

--- solution56/solution56.js
+++ solution56/solution56.js
@@ -1,1 +1,8 @@
+const isLeapYear = (year) => {
+  if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
 export default isLeapYear;
