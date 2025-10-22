0ed428bc7a99e6a4326058128ecf0af74896d7e1
jashka34 <a_cherniadiev@mail.ru>
2025-10-22 20:33:11 +0300

    60/72

--- solution60/solution60.js
+++ solution60/solution60.js
@@ -1,5 +1,11 @@
 // BEGIN (write your solution here)
-
+const normalizeUrl = (url) => {
+  if (url.startsWith("https://")) {
+    return url;
+  } else {
+    return `https://${url}`;
+  }
+};
 // END
 
 export default normalizeUrl;
