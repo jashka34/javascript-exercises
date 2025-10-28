9f868312f38550ec503360819bf20ec79e97e17c
jashka34 <a_cherniadiev@mail.ru>
2025-10-28 21:09:03 +0300

    63/72

--- solution63/solution63.js
+++ solution63/solution63.js
@@ -1,1 +1,18 @@
+const getNumberExplanation = (n) => {
+  let ret;
+  switch (n) {
+    case 666:
+      ret = "devil number";
+      break;
+    case 42:
+      ret = "answer for everything";
+      break;
+    case 7:
+      ret = "prime number";
+      break;
+    default:
+      ret = "just a number";
+  }
+  return ret;
+};
 export default getNumberExplanation;
