15337f30db2ba7d0fbbc40abd5e24046775df857
jashka34 <a_cherniadiev@mail.ru>
2025-11-27 19:58:16 +0300

    70/72

--- solution70/solution70.js
+++ solution70/solution70.js
@@ -1,5 +1,13 @@
 // BEGIN (write your solution here)
-const filterString = () => {};
+const filterString = (s, c) => {
+  let res = "";
+  s.split("").forEach((ch) => {
+    if (ch !== c) {
+      res += ch;
+    }
+  });
+  return res;
+};
 // END
 
 export default filterString;
